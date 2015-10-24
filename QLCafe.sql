ce89aed986b590e4a37930a4212a6d9bccea52e2
Tran Nguyen Ba Phuoc <nguyenbaphuoctran@gmail.com>
2015-10-24 14:37:52 +0700

    Source

---
+++ QLCafe.sql
@@ -0,0 +1,58 @@
+create database QLCafe
+go
+use QLCafe
+go
+create table KhachHang(maKH varchar(10) primary key,
+tenKH nvarchar(255) not null,
+thanthiet bit not null)
+go
+
+create table NhanVien(maNV varchar(10) primary key,
+tenNV nvarchar(255) not null,
+phai nvarchar(10) not null,
+diachi nvarchar(255),
+cmnd varchar(10) not null,
+matkhauNV varchar(20))
+go
+
+create table Coupon(maCP varchar(10) primary key,
+noidung nvarchar(200),
+giam int not null,
+ngayBatDau date not null,
+ngayKetThuc date not null)
+go
+
+create table HoaDon(maHD varchar(10) primary key,
+maNV varchar(10) references NhanVien(maNV),
+ngaylapHD date not null,
+loaiHD bit not null,
+tongtien money,
+maKH varchar(10) references KhachHang(maKH),
+maCP varchar(10) references Coupon(maCP))
+go
+ 
+create table SanPham(maSP varchar(10) primary key,
+tenSP nvarchar(255) not null,
+dongia money not null,
+mota nvarchar(255),)
+go
+
+create table CT_HoaDonThu(maHDT varchar(10) references HoaDon(maHD),
+maSP varchar(10) references SanPham(maSP),
+soluong int not null,
+dongia money)
+go
+
+create table NguyenLieu(maNL varchar(10) primary key,
+tenNL nvarchar(255) not null,
+dongiaNL money)
+go
+
+create table CT_HoaDonChi(maHDC varchar(10) references HoaDon(maHD),
+maNL varchar(10) references NguyenLieu(maNL),
+soluong int not null,
+ghichu nvarchar(255)) 
+go
+
+insert into NhanVien values('Admin',N'Administrator','Other','','','admin')
+go
